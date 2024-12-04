1de83437b9f115ecf8b9dbcdf6bcebd4cafe6f59
Muhammad Usman <ahmadkhan75830@gmail.com>
2024-12-04 14:43:31 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: FlavorConfig.instance.values.appName,
       builder: (_, child) {
         return FlavorBanner(
